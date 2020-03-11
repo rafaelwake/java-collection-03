b0143cc911894f4bb28e7e1e7bcac6f4cd781c17
rafaelwake <rafaelwake07@gmail.com>
2020-03-11 20:52:22 -0300

    exercicio não concluído

---
+++ java-collection-03/src/br/fai/java/Animal.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.fai.java;
+
+/**
+ *
+ * @author Rafael
+ */
+public abstract class Animal {
+
+    
+    private String cor = "preto";
+    private final int idade = 5;
+    private String nome = "nao definido";
+    
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
+    
+    public void som(){
+        System.out.println("Este é o som padrão "
+        + "de um animal: zzzzzzZZZZZZZZZZzzzzzzzz");
+    }
+    
+    public void dizerMeuNome(){
+        
+        
+        System.out.println("O nome do animal eh: xxx" );
+    }
+    
+    private void dizerMinhaIdade(){
+        System.out.println("Minha idade eh: xxx");
+    }
+    
+    
+    
+}
--- java-collection-03/src/br/fai/java/Main.java
+++ java-collection-03/src/br/fai/java/Main.java
@@ -10,5 +10,15 @@ package br.fai.java;
  * @author Rafael
  */
 public class Main {
+    public static void main (String[] args){
+        Main app = new Main();
+        app.start();
+            
+    }
+
+    private void start() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+  
     
 }
